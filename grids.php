166942c4830946289c7105ee900d8cf10615bc63
Martin Spain <martin@straightsixmedia.net>
2017-06-27 21:40:20 +0100

    Updated homepage
    - Removed all the PHP crap to show the grid
    - Added copy and truck image

--- grids.php
+++ grids.php
@@ -1,1 +1,14 @@
-<h1> pie</h1>
+<h1>Get your face on our truck!</h1>
+<p>In 2017 Mission Motorsport is offering everyone the chance to be a part of our story by putting your face (or anything else within reason) on the side of our race truck.</p>
+
+<img src="/assets/img/mm-truck.jpg" class="image-full">
+
+<p>Pictured [above/below/to the side] is the MM Iveco race truck that accompanies us to all events. It hauls cars, bikes, kit, wheelchairs, beneficiaries, brew kit, sticky buns and more. It’s centre stage at the Mission Motorsport Anniversary, Goodwood Festival of Speed, the Mission Motorsport Invitational, the Race of Remembrance, rounds of British GT, Lotus Cup and track and experience days up and down the UK on an almost weekly basis. It visits Spa and the Nürburgring regularly and a number of new initiatives in 2018/9 will see it getting around the country even more.</p>
+
+<p>For a contribution of as little as a tenner* you can own a little piece of that moving real estate for a whole year. Starting at the 2017 Festival of Speed we will be selling space to supporters. You can upload a photo of yourself, something you like, a corporate logo or image – the sky’s the limit.</p>
+
+<p>Once all of the matrix of available space has been sold we will amalgamate the images and pass it to the in-house Mission Motorsport livery studio. Using their state of the art Roland printing equipment they will create a single huge image that will take pride of place on the side of the race truck for one year.</p>
+
+<p>The matrix is name up of 4800 individual squares each measuring 10x10 pixels, and cost £10 per square. Whether you choose one square or ten, the MM livery team can faithfully scale your image so it’s displayed perfectly.</p>
+
+<a href="/getpixel.php" class="btn btn-primary">Buy pixels now <span class="glyphicon glyphicon-menu-right"></span></a>
