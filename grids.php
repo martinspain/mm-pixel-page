39e741751b8536f08451c2156b74a83c9d66432b
Martin Spain <martin@straightsixmedia.net>
2017-06-27 21:44:23 +0100

    Minor copy tweaks to homepage

--- grids.php
+++ grids.php
@@ -1,11 +1,11 @@
 <h1>Get your face on our truck!</h1>
 <p>In 2017 Mission Motorsport is offering everyone the chance to be a part of our story by putting your face (or anything else within reason) on the side of our race truck.</p>
 
-<img src="/assets/img/mm-truck.jpg" class="image-full">
+<p><img src="/assets/img/mm-truck.jpg" class="image-full"></p>
 
-<p>Pictured [above/below/to the side] is the MM Iveco race truck that accompanies us to all events. It hauls cars, bikes, kit, wheelchairs, beneficiaries, brew kit, sticky buns and more. It’s centre stage at the Mission Motorsport Anniversary, Goodwood Festival of Speed, the Mission Motorsport Invitational, the Race of Remembrance, rounds of British GT, Lotus Cup and track and experience days up and down the UK on an almost weekly basis. It visits Spa and the Nürburgring regularly and a number of new initiatives in 2018/9 will see it getting around the country even more.</p>
+<p>Pictured above is the MM Iveco race truck that accompanies us to all events. It hauls cars, bikes, kit, wheelchairs, beneficiaries, brew kit, sticky buns and more. It’s centre stage at the Mission Motorsport Anniversary, Goodwood Festival of Speed, the Mission Motorsport Invitational, the Race of Remembrance, rounds of British GT, Lotus Cup and track and experience days up and down the UK on an almost weekly basis. It visits Spa and the Nürburgring regularly and a number of new initiatives in 2018/9 will see it getting around the country even more.</p>
 
-<p>For a contribution of as little as a tenner* you can own a little piece of that moving real estate for a whole year. Starting at the 2017 Festival of Speed we will be selling space to supporters. You can upload a photo of yourself, something you like, a corporate logo or image – the sky’s the limit.</p>
+<p>For a contribution of as little as a tenner you can own a little piece of that moving real estate for a whole year. Starting at the 2017 Goodwood Festival of Speed we will be selling space to supporters. You can upload a photo of yourself, something you like, a corporate logo or image – the sky’s the limit.</p>
 
 <p>Once all of the matrix of available space has been sold we will amalgamate the images and pass it to the in-house Mission Motorsport livery studio. Using their state of the art Roland printing equipment they will create a single huge image that will take pride of place on the side of the race truck for one year.</p>
 
